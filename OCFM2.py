79fc1fde90175490b65043c45c7c03cdc5aa1c29
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2019-05-26 14:20:38 -0400

    Add some useful things
    Add comments to the code (or most of it, anyway), and make the :open command actually work

--- OCFM2.py
+++ OCFM2.py
@@ -11,12 +11,8 @@
 #
 #Sorry for the bad ASCII art.
 #
-#OCFM attempt 2 - the "command line" way. Made with Python 3.5 to interface
+#OCFM by Ocawesome101. Made with Python 3.5, and works through an interface
 #with Bash. This is strictly command-line and somewhat Vi-like.
-#Hopefully this goes better than my first attempt, which was a MESS.
-#
-#This program will require VLC Media Player and the Mousepad text editor to work
-#to its full extent.
 
 from os import system
 CurrentDir = '/'
@@ -24,27 +20,41 @@ system('ls ' + CurrentDir)
 print('OCFM 2.0. Type \':\' before commands.')
 while 1:
     command = input('')
-    if command == ':q' or command == ':quit' or command == ':exit':
+    if command == ':q' or command == ':quit' or command == ':exit': #Quit command
         exit()
-    elif command == ':/':
-        CurrentDir = '/'
-    elif command == ':h' or command == ':help':
+    elif command == ':/': #Return to root command
+        CurrentDir = '/' #Set current directory to /
+    elif command == ':h' or command == ':help': #Help command
         print('OCFM Help\nCommands:\n:q or :quit or :exit - exit OCFM\n:/ - Return to the root directory\n <Directory Name> - Move to that directory\n:delete <file> - delete a file. Will ask for confirmation.')
-    elif command[0:7] == ':delete':
-        command = command[8:len(command)]
-        delete = input('Are you sure you want to delete file ' + command + '? (y/n)')
-        if delete == 'y':
-            system('rm -r ' + command)
-        elif delete == 'n':
-            print('Aborting')
-        elif delete != 'y' and delete != 'n':
-            print('Aborting - you did not type Y or N.')
-    elif command[0:6] == ':mkdir':
-        command = command[7:len(command)]
-        system('mkdir ' + command)
+        print(':mkdir - makes a directory.\n:bk - moves up a directory.\n:open <file> - open specified file. OCFM will try to recognize the file extension and open the appropriate application. You must NOT specify a directory.')
+    elif command[0:7] == ':delete': #Delete command
+        command = command[8:len(command)] #Remove the ':delete ' from the command variable
+        delete = input('Are you sure you want to delete file ' + command + '? (y/n)') #Ask for confirmation
+        if delete == 'y': #If we have confirmation then
+            system('rm -r ' + command) #delete the file, assuming you have permissions to delete it
+        elif delete == 'n': #If we do not have confirmation then
+            print('Aborting') #abort
+        elif delete != 'y' and delete != 'n': #If answer is not Y or N then
+            print('Aborting - you did not type Y or N.') #abort
+    elif command[0:6] == ':mkdir': #Make directory command
+        command = command[7:(len(command) - 1)] #Remove ':mkdir ' from the command variable
+        system('mkdir ' + command) #Make directory
+    elif command == ':bk' or command == '..' or command == '../': #Go back up a directory, and intercept .. and ../
+        if CurrentDir == '/' or CurrentDir == '//': #If user is already in the root directory then warn them
+            print('You are already in the root directory and cannot go up any further!')
+        else: #If user is NOT in the root directory then
+            CurrentDir = CurrentDir[0:(len(CurrentDir) - 2)] #Remove any '/' characters due to the potential for there to be more than one because of the way my script works
+            while CurrentDir[(len(CurrentDir) - 1)] != '/': #Repeat until we hit a '/' character signifying the upper directory
+                CurrentDir = CurrentDir[0:(len(CurrentDir) - 1)] #Remove one character from the
+#current directory variable. I know this can probably be done in more efficient ways but this is how I have decided to do it.
+    elif command[0:5] == ':open': #Detect if command is :open
+        command = command[6:len(command)] #Remove the ':open' from the command
+        if command[(len(command) - 4):len(command)] == '.txt' or command[(len(command) - 3):len(command)] == '.md' or command[(len(command) - 3):len(command)] == '.sh': #Try to recognize the file extension
+            system('nano ' + CurrentDir + command) #Nano the file
+        else:
+            print('Unknown File Extension!')
     elif command[0] != ':':
         CurrentDir += command + '/'
     elif command[0] == ':':
-        print('UNRECOGNIZED COMMAND - RETURNING TO ROOT DIRECTORY')
-        CurrentDir = '/'
+        print('ERROR - UNRECOGNIZED COMMAND')
     system('ls ' + CurrentDir)
